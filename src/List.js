8d81bc4593f112ad687ef9a2cd521ef7005d6089
Daniel Vanpelt <vanpelt.daniel@yahoo.com>
2020-11-04 17:06:43 -0500

    Initial commit

---
+++ src/List.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import Card from './Card'
+import './List.css';
+
+export default function List(props) {
+  //Is props.header and props.cards the 2 props List accepts?
+  return (
+    <section className='List'>
+      <header className='List-header'>
+        <h2>{props.header}</h2>
+      </header>
+      <div className='List-cards'>
+        {props.cards.map((card) =>
+        //Is this this Card component being rendered? Where is the array generated?
+        <Card
+          key={card.id}
+        //Are these the two instances of Card component where title, content, and key are passed into?
+          title={card.title}
+          content={card.content}
+          />
+        )}
+        <button 
+        type='button'
+        className='List-add-button'
+        >
+          + Add Random Card
+        </button>
+      </div>
+    </section>
+  )
+}
